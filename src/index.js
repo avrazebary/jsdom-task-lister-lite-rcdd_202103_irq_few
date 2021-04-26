2c56a72e382d42febbae165d6ee59f7da1f83f93
Avraz  Zebary <avrazebary@gmail.com>
2021-04-26 09:36:21 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ document.addEventListener("DOMContentLoaded", () => {
   
  const ul = document.getElementById("tasks");
 
-  new_Form.addEventListener("submit", )
+  new_Form.addEventListener("submit", crea)
 });
 
