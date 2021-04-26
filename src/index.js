c6763c5e75a58a1821cad5c3a89cb40bcfcb5bef
Avraz  Zebary <avrazebary@gmail.com>
2021-04-26 09:34:54 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   
-  
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskPriority = document.getElementById("new-task-priority");
+
   
 });
 
